399cdcdfc1607ae6d6fbe8d55bfa010204a8257f
mmtakeuchi <mikemtaki@gmail.com>
2020-10-07 10:14:03 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter( quote => quote.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+      let upvotedQuote = state.find( quote => quote.id === action.quoteId)
+      let indexOfUpvote = state.indexOf(upvotedQuote);
+      upvotedQuote = {...upvotedQuote, votes: upvotedQuote.votes + 1} 
+      let firstHalfCopy =  state.slice(0, indexOfUpvote - 1)
+      let lastHalfCopy = state.slice(indexOfUpvote, state.length - 1)
+      return [...firstHalfCopy, upvotedQuote, ...lastHalfCopy]
+    case "DOWNVOTE_QUOTE":
+      let downvotedQuote = state.find( quote => quote.id === action.quoteId)
+      let indexOfDownvote = state.indexOf(downvotedQuote);
+      if (downvotedQuote.votes > 0) {
+        downvotedQuote = {...downvotedQuote, votes: downvotedQuote.votes - 1}
+      }
+      let firstHalf =  state.slice(0, indexOfDownvote - 1)
+      let lastHalf = state.slice(indexOfDownvote, state.length - 1)
+      return [...firstHalf, downvotedQuote, ...lastHalf]
+    default: 
+      return state
+  }
 }
